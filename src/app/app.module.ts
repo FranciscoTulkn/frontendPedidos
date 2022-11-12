103906b822d5501d289b9db8e4d3b4b1a7e72245
Francisco Javier Tulcán Rodríguez <tufran13@gmail.com>
2022-11-12 01:14:22 -0500

    Frontend-Pedidos-Ciclo4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent,
+    PiePaginaComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
